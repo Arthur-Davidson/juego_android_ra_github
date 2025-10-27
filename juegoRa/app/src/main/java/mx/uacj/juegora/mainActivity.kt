71d458c5466567de12c9f64f6c39297f95c9887f
unknown <padilla.hdz.da00@gmail.com>
2025-10-27 10:15:44 -0600

    [AGREGAR] carpeta del juego

---
+++ juegoRa/app/src/main/java/mx/uacj/juegora/mainActivity.kt
@@ -0,0 +1,35 @@
+package mx.uacj.juegora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.uacj.juegora.ui.theme.JuegoRATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JuegoRATheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JuegoRATheme {
+
+    }
+}
