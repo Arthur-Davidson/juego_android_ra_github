039dc564609136a9a3f404f897dcafab10f5417a
DAVIS MSK <padilla.hdz.da00@gmail.com>
2025-10-28 21:29:22 -0600

    [AGREGAR] las dependencias necesarias

--- juegoRa/app/build.gradle.kts
+++ juegoRa/app/build.gradle.kts
@@ -2,6 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Para integrar KSP
+    alias(libs.plugins.kotlin.ksp)
+
+    // Para integrar en el proyecto Hilt
+    id("com.google.dagger.hilt.android")
+
+    // Plugins para parcelizar
+
+    alias(libs.plugins.jetbrainsKotlinSerialization)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -41,6 +52,24 @@ android {
 
 dependencies {
 
+    // Para integrar Dagger Hilt y sus herramientas necesarias
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.viewmodel)
+    ksp(libs.hilt.android.compilador)
+
+    val androidxhiltCompiler = "1.2.0" // Cambiamos a 1.2.0
+    implementation("androidx.hilt:hilt-work:${androidxhiltCompiler}")
+    ksp("androidx.hilt:hilt-compiler:${androidxhiltCompiler}")
+
+    // Dependencia para Hilt y evitar errores extraños
+    implementation("androidx.work:work-runtime-ktx:2.7.0")
+
+    // Para instalar Retrofit
+    implementation(libs.okhttp)
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+    implementation(libs.login.interceptor)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +78,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Implementa las librerias de Navigation Compose
+    implementation(libs.androidx.navigation.compose.android)
+
+    //Implementa las librerias de Kotlinx para parcelizar (o utilizar JSON)
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
